cd7273ab84b8a673f901d254929381e0478ddc7f
Matteus Varlesse <varlesse04@gmail.com>
2023-06-30 14:16:04 -0300

    feat: add radiix collapsible

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -21,7 +21,7 @@ export function Player() {
             <Video />
           </div>
 
-          <aside className="w-80 absolute top-0 bottom-0 right-0 divide-y-8 divide-zinc-900 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-500">
+          <aside className="w-80 absolute top-0 bottom-0 right-0 divide-y-2 divide-zinc-900 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-500">
             <Module
               moduleIndex={0}
               title="Desvendando o Redux"
