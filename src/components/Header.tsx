a944aa90ea413649dd93224daa7fd81dcbfbb79a
Matteus Varlesse <varlesse04@gmail.com>
2023-07-01 13:56:26 -0300

    feat: aplicando o redux

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useCurrentLesson } from '../store/slices/player'
+
 export function Header() {
+  const { currentLesson, currentModule } = useCurrentLesson()
+
   return (
     <div className="flex flex-col gap-1">
-      <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
+      <h1 className="text-2xl font-bold">{currentLesson.title}</h1>
       <span className="text-sm text-zinc-400">
-        Módulo: Desevendando o Redux
+        Módulo: "{currentModule.title}"
       </span>
     </div>
   )
